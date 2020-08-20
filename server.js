7788b22b89f106d8c9923ea7eaf1008bd202d5ce
teguh baghoy <teguhbaghoy6@gmail.com>
2020-08-20 14:31:54 +0700

    commit yang selanjutnya

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const indexRoutes = require('./routes/index')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb', extended : true}))
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -20,6 +24,7 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/', indexRoutes)
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)

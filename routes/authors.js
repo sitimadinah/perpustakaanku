f665eb1dd2cdd52da60a826ce696fb12171a193d
teguh baghoy <teguhbaghoy6@gmail.com>
2020-08-20 16:06:09 +0700

    test

--- routes/authors.js
+++ routes/authors.js
@@ -10,14 +10,13 @@ router.get('/', async (req, res) => {
     }
     try {
         const authors = await Author.find(searchOptions)
-        res.render('authors/index',  { 
+        res.render('authors/index', {
             authors: authors,
-            searchOptions : req.query
+            searchOptions: req.query
         })
     } catch {
         res.redirect('/')
     }
-
 })
 
 // New Author Route
